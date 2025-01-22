57808cd5c7a8642ee856080a65fe865e1ffc412c
siddharthachilukappli <chilukapallisiddhartha@gmail.com>
2025-01-22 20:19:42 +0530

    Add files via upload

---
+++ Demo012.java
@@ -0,0 +1,18 @@
+class CircleAreaDemo1 {
+
+	void Area() {
+		
+		int r = 4;
+		
+		double result = 3.14* (r*r);
+		
+		System.out.println("The circle area is : " + result);
+	}
+
+}
+public class Demo012 {
+    public static void main(String[] args) {
+        CircleAreaDemo1 obj = new CircleAreaDemo1();
+        obj.Area();
+    }
+}
