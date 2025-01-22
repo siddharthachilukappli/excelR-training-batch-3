4fcb5637470eceac8fdef570b5e3078077260426
siddharthachilukappli <chilukapallisiddhartha@gmail.com>
2025-01-22 20:16:25 +0530

    Add files via upload

---
+++ Demo006.java
@@ -0,0 +1,10 @@
+class Demo006{
+    public static void main(String[] args){
+        String username="jashmika_001";
+        String pwd="jashmikaa_001";
+        while((username=="jashmika_001")&&(pwd=="jashmikaa_001")){
+            System.out.println("hello world");
+            break;
+        }
+    }
+}
