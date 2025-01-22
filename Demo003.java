20c2a265ec41eb452e50fc596eb90a216ea87b15
siddharthachilukappli <chilukapallisiddhartha@gmail.com>
2025-01-22 20:14:29 +0530

    Add files via upload

---
+++ Demo003.java
@@ -0,0 +1,15 @@
+class Demo003{
+    public static void main(String []args){
+        int a=10;
+        int b=20;
+        int c=30;
+        if((a>b)&&(a>c)){
+            System.out.println(a);
+
+        }else if((b>a)&&(b>c)){
+            System.out.println(b);
+        }else{
+            System.out.println(c);
+        }
+    }
+}
