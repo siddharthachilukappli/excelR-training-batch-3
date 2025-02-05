808969983e060aaef3a4060b508a4f13efc34b9a
siddharthachilukappli <chilukapallisiddhartha@gmail.com>
2025-02-06 00:12:18 +0530

    Add files via upload

---
+++ Demo039.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Demo039 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = scanner.next().charAt(0); 
+        if (Character.isUpperCase(ch)) {
+            System.out.println(ch + " is an uppercase letter.");
+        } else if (Character.isLowerCase(ch)) {
+            System.out.println(ch + " is a lowercase letter.");
+        } else if (Character.isDigit(ch)) {
+            System.out.println(ch + " is a digit.");
+        } else {
+            System.out.println(ch + " is neither an uppercase letter, lowercase letter, nor a digit.");
+        }
+        scanner.close();
+    }
+}
+
