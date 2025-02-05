9cbead5073430e713de12e59c9acd6f8d8a3e0bb
siddharthachilukappli <chilukapallisiddhartha@gmail.com>
2025-02-06 00:09:44 +0530

    Add files via upload

--- Demo033.java
+++ Demo033.java
@@ -9,11 +9,15 @@ class LinkedHashSetDemo{
         lhs.add(80);
         System.out.println(lhs);
         lhs.remove(30);
+
+        //forEach loop: It is optimized for Collections and Arrays
 		int sum=0;
 		for(Integer x : lhs) {
 			sum += x;
 		}
+
 		System.out.println("The sum is :" + sum);
+
 		Iterator it = lhs.iterator();
 		while(it.hasNext()) {
 			System.out.println(it.next());
@@ -24,5 +28,5 @@ public class Demo033 {
     public static void main(String[] args) {
         LinkedHashSetDemo obj = new LinkedHashSetDemo();
         obj.linkedHashsetMethod();
-    }
+}
 }
