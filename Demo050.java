520196e677b8ab447aa2369a2792287c973175c6
siddharthachilukappli <chilukapallisiddhartha@gmail.com>
2025-02-06 00:16:03 +0530

    Add files via upload

---
+++ Demo050.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class  Demo050{
+    public static void main(String[] args) {
+        System.out.println("5 Times Table:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("5 x " + i + " = " + (5 * i));
+        }
+    }
+}
